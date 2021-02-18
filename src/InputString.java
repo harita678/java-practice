851e27a33cfef59671975b9f397de41fc4e3b3d9
Harita <harita.678@gmail.com>
2021-02-17 22:30:12 -0500

    add functionality

--- src/InputString.java
+++ src/InputString.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
+
 public class InputString {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("hi, how are you?");
+        String s1 = sc.nextLine();
+    }
+
 }
